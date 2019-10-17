78ebb7035fa53f8fc159414433bd67f4640cf74a
ZK <762845835@qq.com>
2019-10-17 23:29:21 +0800

    add

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const productionGzipExtensions = ['js', 'css'];
 const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-    publicPath: './',
+    publicPath: '/baoleme/',
     productionSourceMap: false,
     devServer: {
         https:false,
